b081b4e6244a27a316f45201934e7b66de8e0450
Eytan Anjel <anjel@usc.edu>
2014-02-11 09:45:44 -0800

    done

---
+++ shuffle.rb
@@ -0,0 +1,16 @@
+number_of_times = ARGV[0].to_i
+array = []
+number_of_times.times do |n|
+  array << n-(number_of_times/2)
+end
+print array.join(' ')
+puts ""
+for i in 0...array.length
+  random = rand(number_of_times-i)+i 
+  temp = array[i]
+  array[i] = array[random]
+  array[random] = temp
+end
+
+print array.join(' ')
+puts ""
